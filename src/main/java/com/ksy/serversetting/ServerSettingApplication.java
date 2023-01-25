6c7356432ea5a3ccebab5295c5a02f4c1e2475c4
youss95 <usekim7555@gmail.com>
2023-01-25 22:25:48 +0900

    commit

---
+++ src/main/java/com/ksy/serversetting/ServerSettingApplication.java
@@ -0,0 +1,13 @@
+package com.ksy.serversetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSettingApplication.class, args);
+    }
+
+}
